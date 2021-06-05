848783426d22efe3747f3079e6958fb0cbf7d6a2
IdotMaster1 <48019206+IdotMaster1@users.noreply.github.com>
2021-06-05 15:17:21 +0200

    funni

--- main.py
+++ main.py
@@ -129,6 +129,10 @@ def furim(event):
 @bot.trigger(lambda event: event.MSG and event.msg.startswith("$abbix"))
 def abbix(event):
      bot.msg(event.chat, f"haha noob, I use scheme to take over the world...")
+        
+        @bot.trigger(lambda event: event.MSG and event.msg.startswith("$idotmaster1"))
+def idotmaster1(event):
+    bot.msg(event.chat, f"haha idolmaster XDXDXDXD so funni!!!!")
 
                 
 bot.connect()
